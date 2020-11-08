ee2ffea2fe62c2ebec42ac13c81f86bb676607b0
sudachi0114 <jnevariste@gmail.com>
2020-11-08 22:19:57 +0900

    最後のセリフを追加

--- src/main/main.go
+++ src/main/main.go
@@ -14,7 +14,11 @@ func main()  {
 	fmt.Printf("フン。%sというのかい。贅沢な名だねェ。\n", name)
 
 	nameArray := strings.Split(name, "")
-	fmt.Println(nameArray, len(nameArray))
+	// fmt.Println(nameArray, len(nameArray))
+
 	var r int = rand.Intn(len(nameArray))
-	fmt.Println(r)
+	// fmt.Println(r)
+
+	newname := nameArray[r]
+	fmt.Printf("今からお前の名前は%sだ。いいかい、%s。わかったら返事をするんだ、%s!!\n", newname, newname, newname)
 }
