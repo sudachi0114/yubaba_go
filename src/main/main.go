c7f15841cdce2a77d66deba9962c335296743482
sudachi0114 <jnevariste@gmail.com>
2020-11-08 22:29:54 +0900

    stdin を復活

--- src/main/main.go
+++ src/main/main.go
@@ -10,8 +10,8 @@ import (
 func main()  {
 	fmt.Println("契約書だよ。そこに名前を書きな。")
 
-	var name string = "すだち"
-	// _, _ = fmt.Scan(&name)
+	var name string
+	_, _ = fmt.Scan(&name)
 	fmt.Printf("フン。%sというのかい。贅沢な名だねェ。\n", name)
 
 	nameArray := strings.Split(name, "")
